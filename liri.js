9b4b96fc5b75e424ce07d5d964e1bf49bfde2cd2
KRISTEN KIRCHGESLER <kristen@KRISTENs-MBP.attlocal.net>
2018-04-06 22:29:44 -0400

    added code for tweets

--- liri.js
+++ liri.js
@@ -1,21 +1,29 @@
  // Read and set environment variables
 var configDotenv = require('dotenv').config();
-
 var keys = require('./keys.js');
+
 var Twitter = require('twitter');
-//var spotify = new Spotify(keys.spotify);
-
-
+var twitterHandle = 'clemmie0913';
 var client = new Twitter(keys.twitter);
 
 
 
+function myTweets() {
+    client.get('statuses/user_timeline', twitterHandle, function(error, tweets, response) {
+        if (!error && response.statusCode == 200) {
+            console.log(' ');
+            console.log('Latest Tweets:');
+            for (i = 0; i < tweets.length; i++) {
+                var number = i + 1;
+                console.log(' ');
+                console.log([i + 1] + '. ' + tweets[i].text);
+                console.log('Created on: ' + tweets[i].created_at);
+                console.log(' ');
+            }
+        }
+    });
+}
 
-var params = {screen_name: 'clemmie0913'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-}); 
+myTweets();
 
 
