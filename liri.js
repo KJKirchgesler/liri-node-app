f302df5d82faa8e0f9509448a15d0d0374fdf489
KRISTEN KIRCHGESLER <kristen@KRISTENs-MBP.attlocal.net>
2018-04-08 17:54:48 -0400

    added Spotify code and updated command switch statement

--- liri.js
+++ liri.js
@@ -2,38 +2,72 @@
 var configDotenv = require('dotenv').config();
 var keys = require('./keys.js');
 
+//set Twitter variables
 var Twitter = require('twitter');
-var twitterHandle = 'clemmie0913';
+var params = {screen_name: 'clemmie0913', count: 20}; 
 var client = new Twitter(keys.twitter);
-var commands = process.argv[2];
 
+//set Spotify variables
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
+//setting the command and input variables from the process.argv array
+var command = process.argv[2];
+var input = process.argv[3];
 
+//Function to grab the last 20 tweets for the my-tweets command
 function myTweets() {
-    client.get('statuses/user_timeline', twitterHandle, function(error, tweets, response) {
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error && response.statusCode == 200) {
-            console.log(' ');
+            console.log('=================================');
             console.log('Latest Tweets:');
             for (i = 0; i < tweets.length; i++) {
                 var number = i + 1;
-                console.log(' ');
+                console.log('=================================');
                 console.log([i + 1] + '. ' + tweets[i].text);
                 console.log('Created on: ' + tweets[i].created_at);
-                console.log(' ');
+                console.log('=================================');
             }
         }
     });
 }
 
-switch (commands) {
-    case "my-tweet":
-    case "my-tweets":
-    case "getTweet":
-    case "getTweets":
-    case "tweet":
-    case "tweets":
-    case "twitter":
+//creating the function for the spotify-this-song-command
+function spotifyThis(input) {
+    // If the user fails to specify a song, return "The Sign" by Ace of Base.
+    if (input == null) {
+        input = 'The Sign Ace of Base';
+    }
+    
+    
+    // Create the search of the spotify and input our parameters
+    spotify.search({ type: 'track', query: input }, function(error, data) {
+        if (!error && input != null) {
+          for (var i = 0; i < data.tracks.items.length; i++) {
+            var artists = data.tracks.items[i].artists[0].name; //This took forever to find in the API documentation
+            var name = data.tracks.items[i].name;
+            var preview = data.tracks.items[i].preview_url;
+            var album = data.tracks.items[i].album.name;
+            console.log('=================================');
+            console.log('Artist: ' + artists);
+            console.log('Song Title: '+ name);
+            console.log('Preview URL: '+ preview);
+            console.log('Album Title: '+ album);
+            console.log('=================================');
+          }  
+        } else {
+            console.log("Error occurred: " + error);
+         }
+    })
+}
+
+//Switch set up to call the functions for each of LIRI's commands
+switch (command) {
+    case 'my-tweets':
         myTweets();
+        break;
+    case 'spotify-this-song':
+        spotifyThis(input);
         break;
     default: 
 		console.log("That's not a valid command");
